7d40592536d7e60b27713cfe0d37f80dc54145d4
BanditBloodwyn <kevin.eichenberg.info@gmail.com>
2024-07-18 20:41:04 +0200

    CommandsPanel draft

---
+++ LogAnalyzer.ViewModels/Navigation/CommandsPanelViewModel.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.ObjectModel;
+
+namespace LogAnalyzer.ViewModels.Navigation;
+
+public class CommandsPanelViewModel : ViewModelBase
+{
+    public ObservableCollection<string> Commands { get; set; } = ["test", "tsrg"];
+}
