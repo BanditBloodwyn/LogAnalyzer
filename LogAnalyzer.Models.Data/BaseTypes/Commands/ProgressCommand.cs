ee3011e811ec417dd51c889d16ea757d00cd2bc8
BanditBloodwyn <kevin.eichenberg.info@gmail.com>
2024-07-19 07:05:38 +0200

    added ProgressCommand base class, updated CommandsPanelViewModel

---
+++ LogAnalyzer.Models.Data/BaseTypes/Commands/ProgressCommand.cs
@@ -0,0 +1,15 @@
+﻿using LogAnalyzer.Models.Data.Interfaces;
+
+namespace LogAnalyzer.Models.Data.BaseTypes.Commands;
+
+public abstract class ProgressCommand(string name) : IQueuedCommand
+{
+    public string Name { get; } = name;
+
+    public IProgress<int>? PercentsProgress { get; set; }
+    public IProgress<string>? MessageProgress { get; set; }
+
+    public bool AllowParallelExecuting { get; set; }
+
+    public abstract Task Execute();
+}
--- LogAnalyzer.ViewModels/Navigation/CommandsPanelViewModel.cs
+++ LogAnalyzer.ViewModels/Navigation/CommandsPanelViewModel.cs
@@ -1,8 +1,22 @@
-﻿using System.Collections.ObjectModel;
+﻿using LogAnalyzer.Core.EventBus;
+using LogAnalyzer.Models.Data.BaseTypes.Commands;
+using LogAnalyzer.Models.Events;
+using System.Collections.ObjectModel;
 
 namespace LogAnalyzer.ViewModels.Navigation;
 
 public class CommandsPanelViewModel : ViewModelBase
 {
-    public ObservableCollection<string> Commands { get; set; } = ["test", "tsrg"];
+    public ObservableCollection<ProgressCommand> Commands { get; set; } = [];
+
+    public CommandsPanelViewModel()
+    {
+        EventBinding<AddNewProgressCommandEvent> addNewCommandEventBinding = new(OnAddCommand);
+        EventBus<AddNewProgressCommandEvent>.Register(addNewCommandEventBinding);
+    }
+
+    private void OnAddCommand(AddNewProgressCommandEvent @event)
+    {
+        Commands.Add(@event.Command);
+    }
 }
