8294d8bc67604a4c93f1f6c0b7c359b1d584cfec
BanditBloodwyn <kevin.eichenberg.info@gmail.com>
2024-08-06 00:45:33 +0200

    Design VM for feature button panel

---
+++ LogAnalyzer.ViewModels.Design/Navigation/FeatureButtonsPanelDesignViewModel.cs
@@ -0,0 +1,9 @@
+﻿using LogAnalyzer.ViewModels.MainFeatures;
+using LogAnalyzer.ViewModels.MainFeatures.LogAnalysis;
+using LogAnalyzer.ViewModels.Navigation;
+
+namespace LogAnalyzer.ViewModels.Design.Navigation;
+
+public class FeatureButtonsPanelDesignViewModel() : FeatureButtonsPanelViewModel(
+    new LogAnalysisMainViewModel(null!, null!),
+    new SettingsMainViewModel(null!));
