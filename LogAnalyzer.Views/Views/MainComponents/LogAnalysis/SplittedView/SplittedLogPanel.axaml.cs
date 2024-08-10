f90db2b4abe15ca7dd11dda3599405c6f2159bdf
BanditBloodwyn <kevin.eichenberg.info@gmail.com>
2024-08-10 16:48:24 +0200

    moved classes

--- LogAnalyzer.Views/Views/MainComponents/LogAnalysis/SplittedView/SplittedLogPanel.axaml.cs
+++ LogAnalyzer.Views/Views/MainComponents/LogAnalysis/SplittedView/SplittedLogPanel.axaml.cs
@@ -1,8 +1,6 @@
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
 
-namespace LogAnalyzer.Views;
+namespace LogAnalyzer.Views.Views.MainComponents.LogAnalysis.SplittedView;
 
 public partial class SplittedLogPanel : UserControl
 {
