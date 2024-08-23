7e1e808e359bd7903b1e28127b3e41ec657e2d12
Kevin Eichenberg <kevin.eichenberg@atbas.de>
2024-08-23 11:55:51 +0200

    code cleanup

--- LogAnalyzer.Models.Data/Containers/LogEntry.cs
+++ LogAnalyzer.Models.Data/Containers/LogEntry.cs
@@ -1,18 +1,11 @@
-﻿using LogAnalyzer.Core;
-
-namespace LogAnalyzer.Models.Data.Containers;
+﻿namespace LogAnalyzer.Models.Data.Containers;
 
 public readonly record struct LogEntry(
     long index,
-    string timeStamp, 
-    string source, 
-    string logType, 
-    string message, 
-    string innerMessage, 
+    string timeStamp,
+    string source,
+    string logType,
+    string message,
+    string innerMessage,
     int fileIndex,
-    RepositoryInteractionInformation? repositoryInteractionInformation = null)
-{
-    public RepositoryInteractionInformation? RepositoryInteractionInformation { get; } = repositoryInteractionInformation;
-
-    public bool HasInnerMessage => !string.IsNullOrEmpty(innerMessage);
-}
+    RepositoryInteractionInformation? repositoryInteractionInformation = null);
--- LogAnalyzer.ViewModels/MainFeatures/LogAnalysis/LogEntryViewModel.cs
+++ LogAnalyzer.ViewModels/MainFeatures/LogAnalysis/LogEntryViewModel.cs
@@ -1,14 +1,10 @@
-﻿using Avalonia.Animation;
-using LogAnalyzer.Core.ViewsModels;
+﻿using LogAnalyzer.Core.ViewsModels;
 using LogAnalyzer.Models.Data.Containers;
 
 namespace LogAnalyzer.ViewModels.MainFeatures.LogAnalysis;
 
 public class LogEntryViewModel(LogEntry _logEntry) : ViewModelBase
 {
-    public LogEntryViewModel()
-        : this(default) {}
-    
     public long Index => _logEntry.index;
     public string TimeStamp => _logEntry.timeStamp;
     public string Source => _logEntry.source;
@@ -17,4 +13,6 @@ public class LogEntryViewModel(LogEntry _logEntry) : ViewModelBase
     public string InnerMessage => _logEntry.innerMessage;
     public int FileIndex => _logEntry.fileIndex;
     public RepositoryInteractionInformation? RepositoryInteractionInformation => _logEntry.repositoryInteractionInformation;
+
+    public bool HasInnerMessage => !string.IsNullOrEmpty(InnerMessage);
 }
